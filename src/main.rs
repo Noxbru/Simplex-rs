c81f3c619b4aad46e3c650b9d089dc0417d60254
Amila Welihinda <amilajack@gmail.com>
2018-12-08 17:36:12 -0800

    format with rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 #[macro_use]
 extern crate derive_more;
 
-mod simplex;
 mod point;
+mod simplex;
 
 use point::Point;
 
 fn main() {
-    let fun = |p: Point| { p.x.cos() * p.y.sin() * (-(p.x*p.x+p.y*p.y) / 10.0).exp() };
+    let fun = |p: Point| p.x.cos() * p.y.sin() * (-(p.x * p.x + p.y * p.y) / 10.0).exp();
 
-    let pp = [Point { x: 0.5, y: 0.0 },
-              Point { x:-2.0, y: 2.0 },
-              Point { x: 0.0, y:-2.0 }];
+    let pp = [
+        Point { x: 0.5, y: 0.0 },
+        Point { x: -2.0, y: 2.0 },
+        Point { x: 0.0, y: -2.0 },
+    ];
 
-    let yy = [fun(pp[0]),
-              fun(pp[1]),
-              fun(pp[2])];
+    let yy = [fun(pp[0]), fun(pp[1]), fun(pp[2])];
 
     let mut s = simplex::Simplex {
         f: fun,
